ed2563c40a3dcdc631209bb9a8a20162a4334ead
Ashkar-Husain <ashkarhusain72@gmail.com>
2026-01-21 22:30:36 +0530

    template is created using css

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="todo">
+          <div id="form">
+            <div className="todo-app"><span>Todo App</span></div>
+            <input type="text" id='title' className='form-control' />
+            <button className='btn'>Submit</button>
+            <div id="listItems">
+              <ul>
+                <li>I have to message John <span>Completed</span></li>
+                <li>I hav to eat some food <span>Completed</span></li>
+                <li>I have to call Saifi <span>Completed</span></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
