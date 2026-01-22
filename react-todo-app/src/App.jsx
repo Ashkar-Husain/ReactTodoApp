48894b17034984ade6e33a801582e8b6328454fd
Ashkar-Husain <ashkarhusain72@gmail.com>
2026-01-22 23:44:40 +0530

    add and update todo with storing values in localstorage

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,7 +1,58 @@
 import './App.css'
-
+import { useEffect, useState } from 'react'
 function App() {
-  
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState(() => {
+    try {
+      const storedTodos = JSON.parse(localStorage.getItem('reactTodos'));
+      return storedTodos || [];
+    } catch {
+      return [];
+    }
+  });
+
+
+
+  useEffect(() => {
+    localStorage.setItem('reactTodos', JSON.stringify(todos));
+  }, [todos]);
+
+
+  function handleSubmit() {
+    if (!todo.trim()) {
+      alert("Todo is required!");
+      return;
+    }
+
+    const newTodo = {
+      title: todo,
+      isCompleted: "Pending"
+    }
+
+    setTodos(prev => [...prev, newTodo])
+
+    setTodo('');
+
+  }
+
+  function toggleIsCompleted(index) {
+    setTodos(prevTodos => {
+      return prevTodos.map((todo, idx) => {
+        if (idx === index) {
+          return {
+            ...todo,
+            isCompleted:
+              todo.isCompleted === "Pending" ? "Completed" : "Pending"
+          };
+        }
+        return todo;
+      });
+    });
+  }
+
+
+
+
 
   return (
     <>
@@ -9,13 +60,18 @@ function App() {
         <div className="todo">
           <div id="form">
             <div className="todo-app"><span>Todo App</span></div>
-            <input type="text" id='title' className='form-control' />
-            <button className='btn'>Submit</button>
+            <input type="text" id='title' className='form-control'
+              value={todo} onChange={(e) => setTodo(e.target.value)} />
+            <button className='btn' onClick={() => handleSubmit()}>Submit</button>
             <div id="listItems">
               <ul>
-                <li>I have to message John <span>Completed</span></li>
-                <li>I hav to eat some food <span>Completed</span></li>
-                <li>I have to call Saifi <span>Completed</span></li>
+                {
+                  todos.map((todo, index) => (
+                    <li key={index}>{todo.title} <span style={todo.isCompleted == 'Pending' ? { color: 'red' } : { color: 'green' }} onClick={() => toggleIsCompleted(index)}>{todo.isCompleted}</span></li>
+                  ))
+                }
+
+
               </ul>
             </div>
           </div>
